a2cab3213dd96d49fc9995d50134026d64009e25
paco <paco@localhost.com>
2025-11-21 18:03:26 -0600

    inicio

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
+
+@Controller('cars')
+// @UsePipes(ValidationPipe)
+export class CarsController {
+  constructor(
+    private readonly carsService:CarsService
+  ) {
+  }
+
+  @Get()
+  getAllCars(){
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  getCarById( @Param('id', new ParseUUIDPipe({version: '5'})) id:string ){
+    console.log({id});
+    return this.carsService.findOneById(id);
+  }
+
+  @Post()
+  createCar(@Body() createCarDto:CreateCarDto){
+    return createCarDto;
+  }
+  @Patch(':id')
+  updateCar(
+    @Param('id', ParseIntPipe) id:number,
+    @Body() body:any
+  ){
+    return body;
+  }
+  @Delete(':id')
+  deleteCar( @Param('id', ParseIntPipe) id:number ){
+    return {
+      method:'DELETE',
+      id
+    };
+  }
+}
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {v4 as uuid} from 'uuid';
+import { Car } from './interfaces/car.interface';
+
+@Injectable()
+export class CarsService {
+  private cars:Car[] = [
+    {
+      id: uuid(),
+      brand: 'ford',
+      model: 'fiesta'
+    },
+    {
+      id: uuid(),
+      brand: 'porsche',
+      model: '911 Turbo'
+    }
+  ];
+
+  findAll(){
+    return this.cars;
+  }
+
+  findOneById(id:string){
+    const car = this.cars.find(item => item.id == id);
+    if(!car){
+      throw new NotFoundException(`Car with ID ${id} not found`);
+    }
+    return car;
+  }
+}
